ba4c5f5c594e5b72f5df04c560becdf84d27ea04
manuelalejandroms <mmena@truora.com>
2019-05-19 21:11:04 -0500

    DAO

--- servers.go
+++ servers.go
@@ -1,20 +1,57 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  "log"
-  _ "github.com/lib/pq"
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
 )
 
+// Server ...
+type Server struct {
+	address  string
+	sslGrade string
+	country  string
+	owner    string
+}
+
+// ServerTest ...
+type ServerTest struct {
+	testHour       string // MM - DD - YYYY hh:mm:ss
+	testInProgress bool
+	servers        []Server
+	sslGrade       string
+	isDown         bool
+}
+
+// ServerTestComplete ...
+type ServerTestComplete struct {
+	servers          []Server
+	sslGrade         string
+	previousSslGrade string
+	logo             string
+	title            string
+	isDown           bool
+}
+
+// DAOInterface ...
+type DAOInterface interface {
+	initTable()
+	selectInDB()
+	createInDB()
+	updateInDB()
+	deleteInDB()
+}
+
 func main() {
-  // Connect to the "servers" database.
-  db, err := sql.Open("postgres",
-      "postgresql://manuelams@localhost:26257/bank?ssl=true&sslmode=require&sslrootcert=../certs/ca.crt&sslkey=../certs/client.maxroach.key&sslcert=../certs/client.maxroach.crt")
-  if err != nil {
-      log.Fatal("error connecting to the database: ", err)
-  }
-  fmt.Println("Hello world")
-  defer db.Close()
+	// Connect to the "servers" database.
+	db, err := sql.Open("postgres",
+		"postgresql://manuelams@localhost:26257/servers?ssl=true&sslmode=require&sslrootcert=../certs/ca.crt&sslkey=../certs/client.maxroach.key&sslcert=../certs/client.maxroach.crt")
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+	fmt.Printf("Hello world %T\n", db)
+	defer db.Close()
 
 }
